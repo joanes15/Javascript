a135c984a79214f5c49a986e38b633699d14c114
quewueisso <103038625+joanes15@users.noreply.github.com>
2024-08-14 11:31:15 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+Function carregar(){
+var msg = window.document .getElementById('msg')
+var img =window.document.getElementById (' imagem')
+var data = new Date() //pega a data atual
+var hora -data.getHours ()//pega a hora da data atual
+msg.innerHTML = Agora sao ${hora} horas.
+if (hora >=0 && hora < 12){
+//Bom dia
+img.src = 'manha.jpeg'
+} else if (hora >=12 && hora <18){
+1/Boa tarde
+img.srC = 'tarde.jpeg'
+}else {
+//Boa noite
+img. srC = 'noite. j peg"
+}
